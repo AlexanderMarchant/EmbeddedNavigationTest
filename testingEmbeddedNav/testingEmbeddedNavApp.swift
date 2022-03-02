fa2b2625a827ed8f303d5fe426ae993df8a0ccf5
Alex Marchant <alex_marchant@outlook.com>
2022-03-02 17:59:02 +0100

    Initial Commit

---
+++ testingEmbeddedNav/testingEmbeddedNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingEmbeddedNavApp.swift
+//  testingEmbeddedNav
+//
+//  Created by Alex Marchant on 02/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct testingEmbeddedNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
